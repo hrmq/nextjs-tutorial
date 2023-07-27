d6d4baf3ce258857f9cf19bdb5b335c7074bd171
HasanR <hrahimi@360insights.com>
2023-07-27 11:59:48 -0400

    navigating programatically

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import Link from 'next/link'
+import { useRouter } from "next/router"
+import Link from "next/link"
 
 export default function Home() {
+  const router = useRouter()
+
+  const handleClick = () => {
+    // router.push('/product')
+    router.replace('/product')
+  }
+
   return (
     <>
       <div>Home</div>
       <Link href='/blog'>Blog</Link><br/>
-      <Link href='/product'>Product</Link>
+      <Link href='/product'>Product</Link><br />
+      <button onClick={handleClick}>
+        Place Order
+      </button>
     </>
   )
 }
