8fafe9c5264735b7abe60474022c78c1428e7add
HasanR <hrahimi@360insights.com>
2023-08-10 11:59:00 -0400

    component level style

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import styles from '@/styles/About.module.css'
 
 export default function About() {
   return (
     <div>
+      <div className={styles.highlight}> About Page </div>
       <button className='btn btn-success'>Primary</button>
     </div>
   )
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import styles from '@/styles/Contact.module.css'
 
 export default function Contact() {
   return (
-    <div><h2>Contact</h2></div>
+    <div className={styles.highlight}>Contact</div>
   )
 }
