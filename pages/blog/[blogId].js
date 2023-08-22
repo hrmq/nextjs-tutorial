2eb28a28a966bb3eb7c0bc88f7210a5920da2552
HasanR <hrahimi@360insights.com>
2023-08-22 09:49:13 -0400

    local envs

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -8,12 +8,20 @@ export default function Blog({ title, description }) {
             <meta name="description" content={description} />
         </Head>
         <h1>Article</h1>
+        {/* <h1 className="content">
+            Env User {process.env.DB_USER} Password {process.env.DB_PASSWORD}
+        </h1> */}
+        <h1 className="content">
+            Env Analytics {process.env.NEXT_PUBLIC_ANALYTICS_ID} 
+        </h1>
     </>
   )
 }
 
 
 export async function getServerSideProps() {
+    // const user = process.env.DB_USER
+    // const password = process.env.PASSWORD
     return {
         props: {
             title: 'Article Title',
